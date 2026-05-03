14e62a4e54115e86b7ec7126209f55a9e61983dd
huzwarsi <huzaifawarsi2006@gmail.com>
2026-05-03 12:51:46 +0500

    First Server Beckend

---
+++ First Beckend Server/index.js
@@ -0,0 +1,86 @@
+// console.log('Hello Node js......')
+
+// const http = require('http')
+
+// const server = http.createServer((req, res)=>{
+//     res.end('Server create hogaya ha bhai......')
+// })
+
+// server.listen(5000,'127.0.0.1', ()=>{
+//     console.log('Server create hogaya ha bhai......');
+    
+// })
+
+
+
+
+// const http = require('http')
+// const server =  http.createServer((req,res)=>{
+//     console.log(req,'req==>');
+    
+//   res.end('Now Server is running But its time go for namaz');
+    
+// })
+
+
+// server.listen(5000,'127.0.0.1',()=>{
+//     console.log('server has started');
+    
+// })
+
+
+
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req, res)=>{
+  console.log(req.url,'==>');
+//   if(req.url.startsWith('/cooldrink')){
+//     if(req.url.split('?')[1].split('=')[1] == 'true'){
+//     res.end('Ya lo bhai cool-drink sath ma glass bhi')
+
+//     }else{
+
+//       res.end('Ya lo bhai cool-drink')
+//     }
+//   }
+//   else if (req.url == '/cooldrink'){
+//       res.end('Ya lo bhai cool-drink')
+
+//   }else if(req.url == '/Chai'){
+//     res.end('Ya lo bhai Chai')
+    
+//   }else if(req.url == '/')
+//     {
+//     res.end('kya loo ga bhai...')
+
+//   }
+//   else{
+//     res.end('Soory bhai mera pas available nhi ha...')
+
+//   }
+// })
+
+if(req.url == '/tea'){
+  res.end('Here is your Tea ')
+}else if(req.url == '/coffee'){
+  res.end('Here is your Coffee ')
+  
+}else if(req.url == '/juice'){
+  res.end('Here is your Juice ')
+
+}
+else if(req.url == '/'){
+  res.end('What you want to take')
+
+}
+
+else {
+  res.end('404 bad request')
+
+}
+})
+
+server.listen(5000,'127.0.0.1',()=>{
+  console.log('Hello from server')})
+  
